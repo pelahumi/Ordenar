09bf6c76833342dc2e2989496cc8ef0c0ad641c6
Pelayo <phuermij@myuax.com>
2022-03-06 18:27:18 +0100

    ficheros diagramas

--- ejercicios/topologica.py
+++ ejercicios/topologica.py
@@ -12,8 +12,8 @@ class Topologica():
         #Este bucle recorre los elementos del array empezando por el segundo elemento
         for i in range(1, len(self.array)):
             num = self.array[i]
-            index = i - 1
-            #En este bucle
+            index = i - 1 #Con este índice llamaremos a los elementos anteriores a num
+            #En este bucle comparamos los elementos anteriores a num y si son mas grandes los intercambiamos
             while index >= 0 and num < self.array[index]:
                 self.array[index + 1] = self.array[index]
                 index -= 1
