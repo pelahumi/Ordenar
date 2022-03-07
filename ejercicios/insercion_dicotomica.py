22443b88c2e29aac1f6f56570fe0a63db27724d7
Pelayo <phuermij@myuax.com>
2022-03-07 17:38:54 +0100

    ejercicio2

---
+++ ejercicios/insercion_dicotomica.py
@@ -0,0 +1,19 @@
+#Creamos la clase
+
+class Insercion():
+    pass #ponemos un pass porque definiremos luego los atributos de la clase
+
+    #Definimos el constructor con los atributos de la clase
+
+    def __init__(self, array):
+        self.array = array
+        self.minimo = min(self.array)
+        self.new_array = []
+        self.index = 0
+    
+    #Definimos el método de la clase que nos ordenará el array
+
+    def ordenar(self):
+        if  
+
+        
