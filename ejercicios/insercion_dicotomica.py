c1a985e91a3946851635c31fccfd97fc1a2d5767
Pelayo <phuermij@myuax.com>
2022-03-08 17:59:41 +0100

    cambios

--- ejercicios/insercion_dicotomica.py
+++ ejercicios/insercion_dicotomica.py
@@ -7,27 +7,25 @@ class Insercion():
 
     def __init__(self, array):
         self.array = array
-        self.minimo = min(self.array)
         self.new_array = []
         self.index = 0
     
     #Definimos el método de la clase que nos ordenará el array
 
     def ordenar(self):
+        
+        minimo = min(array)
+
         if self.index <= len(self.array):
-            if  self.array[self.index] <= self.minimo:
-                self.new_array.append(self.array[self.index])
-                self.index += 1
-                self.ordenar()
-                
-            else:
-                self.index += 1
-                self.ordenar()
+            self.new_array.append(minimo)
+            array.remove(minimo)
+            self.ordenar()
         else:
             return "El array ordenado es: {}".format(self.new_array)
 
 if __name__ == "__main__":
     array = [13, 56, 77, 2, 27, 89, 71, 19, 33]
+    print(min(array))
     #Creamos el objeto
     lista = Insercion(array)
     print(lista.ordenar())
