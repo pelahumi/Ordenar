a2fbce874101dc0c62455156774d94b83facbf38
Pelayo <phuermij@myuax.com>
2022-03-13 18:11:21 +0100

    cambios

--- clases/especificaciones.py
+++ clases/especificaciones.py
@@ -7,11 +7,39 @@ class Segmento():
         self.tabla = tabla
 
     #Creamos el primer método
-    def segmentos(self):
+    def especificaciones(self):
         seg = []
 
+        # Bucle que busca los elementos que son mayores al que tienen a su derecha
         for i in range(len(self.tabla) - 1):
+
             if self.tabla[i] >= self.tabla[i + 1]:
                 seg.append(self.tabla[i])
+
+                if self.tabla[i + 1] < self.tabla[i + 2]:
+                    seg.append(self.tabla[i + 1])
+
+                    seg2 = []
+
+                    # Este bucle hace lo mismo que el anterior pero con un rango distinto
+                    for j in range(i + 2, len(self.tabla) - 1):
+
+                        if self.tabla[j] >= self.tabla[j + 1]:
+
+                            seg2.append(self.tabla[j])
+
+                            return seg, seg2
+            
             else:
                 pass
+
+# Método que aplica las especificaciones del enunciado
+def explorar(self, lista):
+
+    # Copia de seguridad
+    mi = lista[0]
+
+    # Mover los elementos un lugar a la izquierda
+    lista = lista[1:] + lista[:1]
+
+    return lista
